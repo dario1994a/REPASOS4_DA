b1b61c3cc8802fff4f4eb978538493cd1038c0e6
master <master@SMLAB306PC17_23>
2023-06-19 21:32:36 -0500

    Agregar archivos de proyecto.

---
+++ REPASOS4_DA/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace REPASOS4_DA
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInicia_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
